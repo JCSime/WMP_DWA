1958484c54a35b7c4af2db67b64684951f6fcdd8
John S <johnc.sime@gmail.com>
2022-01-27 19:35:48 -0500

    Added my random fact generator project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Fact from './components/Fact';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='title'>
+        <h1>Random Facts Generator</h1>
+      </div>
+      <Fact />
     </div>
   );
 }
